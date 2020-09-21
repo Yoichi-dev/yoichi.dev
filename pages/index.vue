51d7b8f29bc2859b248de336c5e10ae552ca6bf0
Yoichi-dev <yoichi.developer@gmail.com>
2020-09-21 19:26:06 +0900

    404修正

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-col class="text-center">
+    <v-tooltip left>
+      <template v-slot:activator="{ on }">
+        <v-btn :href="source" icon large target="_blank" v-on="on">
+          <v-icon large>mdi-code-tags</v-icon>
+        </v-btn>
+      </template>
+      <span>Source</span>
+    </v-tooltip>
+  </v-col>
+</template>
