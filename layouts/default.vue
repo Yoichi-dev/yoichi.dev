63c0edab85667d56a9c420e6d22e0865377d3013
Yoichi-dev <yoichi.developer@gmail.com>
2020-09-21 19:48:24 +0900

    初期デザイン

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,95 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
+  <v-app>
+    <v-navigation-drawer
+      v-model="primaryDrawer.model"
+      :clipped="primaryDrawer.clipped"
+      :floating="primaryDrawer.floating"
+      :mini-variant="primaryDrawer.mini"
+      :permanent="primaryDrawer.type === 'permanent'"
+      :temporary="primaryDrawer.type === 'temporary'"
+      app
+      overflow
+    >
+      <v-container>
+        <v-list-item>
+          <v-list-item-title class="title">Menu</v-list-item-title>
         </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-email</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+        <v-divider></v-divider>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
     </v-navigation-drawer>
 
-    <v-app-bar app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
+    <v-app-bar :clipped-left="primaryDrawer.clipped" app>
+      <v-app-bar-nav-icon
+        v-if="primaryDrawer.type !== 'permanent'"
+        @click.stop="primaryDrawer.model = !primaryDrawer.model"
+      ></v-app-bar-nav-icon>
+      <v-toolbar-title style="cursor: pointer" @click="$router.push('/')" v-text="title" />
     </v-app-bar>
 
     <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <nuxt />
-        </v-row>
-      </v-container>
+      <nuxt />
     </v-main>
-    <v-footer color="indigo" app>
-      <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer :inset="footer.inset" app>
+      <span class="px-4">&copy; {{ new Date().getFullYear() + " " + title }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
-  props: {
-    source: String,
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-pencil-outline',
+          title: 'My Skill',
+          to: '/skill',
+        },
+        {
+          icon: 'mdi-book-open-page-variant',
+          title: 'Blog',
+          to: '/blog',
+        },
+        {
+          icon: 'mdi-folder-plus-outline',
+          title: 'Create App & Site',
+          to: '/create',
+        },
+        {
+          icon: 'mdi-message-processing-outline',
+          title: 'Diary',
+          to: '/diary',
+        },
+      ],
+      title: 'Yoichi.dev',
+      primaryDrawer: {
+        model: null,
+        type: 'default (no property)',
+        clipped: true,
+        floating: false,
+        mini: false,
+      },
+      footer: {
+        inset: true,
+      },
+    }
   },
-  data: () => ({
-    drawer: null,
-  }),
 }
 </script>
+
+<style lang="scss" scoped>
+body {
+  font-family: 'NotoSerifFont';
+}
+</style>
