e5a74d590b05190e49cb9f90b0cf692dd8c2b722
Yoichi-dev <yoichi.developer@gmail.com>
2020-12-10 22:56:44 +0900

    bugfix

--- pages/index.vue
+++ pages/index.vue
@@ -62,19 +62,6 @@
                               />
                             </svg>
                             <div id="room_name"></div>
-                            <!-- <svg
-                                  width="1em"
-                                  height="1em"
-                                  viewBox="0 0 16 16"
-                                  class="bi bi-telephone mt-1"
-                                  fill="currentColor"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                >
-                                  <path
-                                    fill-rule="evenodd"
-                                    d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
-                                  />
-                                </svg> -->
                           </div>
                         </div>
                       </div>
@@ -260,6 +247,7 @@
             ><a
               href="https://www.showroom-live.com/room/profile?room_id=317313"
               target="_blank"
+              rel="noopener"
               >例：ルームIDとは？</a
             ></small
           >
@@ -385,14 +373,16 @@ export default {
       ).slice(-2)}:${('0' + jikan.getMinutes()).slice(-2)}`
     }, 1000)
 
-    if (this.$store.state.backgroundcolor != null) {
-      this.styles.backgroundColor = this.$store.state.backgroundcolor
-    }
+    setTimeout(() => {
+      if (this.$store.state.backgroundcolor != null) {
+        this.styles.backgroundColor = this.$store.state.backgroundcolor
+      }
 
-    if (this.$store.state.roomid != null) {
-      this.roomId = this.$store.state.roomid
-      this.check(this.roomId)
-    }
+      if (this.$store.state.roomid != null) {
+        this.roomId = this.$store.state.roomid
+        this.check(this.roomId)
+      }
+    }, 0)
 
     document.getElementById('comment').onscroll = () => {
       let beforDom = document.getElementById('comment')
@@ -443,6 +433,7 @@ export default {
         })
     },
     saveColor() {
+      console.log(this.styles.backgroundColor)
       this.$store.commit('setBackgroundColor', this.styles.backgroundColor)
     },
     addComments(data) {
